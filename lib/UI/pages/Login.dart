58052d02f892c0e84352e7e2cc1f505d22cc55c4
afyaruro <andresyaruro20@gmail.com>
2022-10-20 11:22:29 -0500

    login terminado y adaptable

--- lib/UI/pages/Login.dart
+++ lib/UI/pages/Login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:proyecto_fast_reservation/UI/CustomPainter/painterLogin.dart';
 // ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
@@ -9,102 +10,113 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Color(0xFF00BBFF),
-          Color(0xFF80DDFF),
-          Color(0xFF00BBFF),
-          //Color(0xFF005E80),
-        ])),
-        child: CustomPaint(
-          painter: loginPainter(),
-          child: Column(children: [
-            Padding(
-              padding: EdgeInsets.only(top: 180, left: 50),
-              child: Row(
-                children: [
-                  Text(
-                    "SIGN IN ",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "/",
-                    style: TextStyle(color: Colors.grey, fontSize: 15),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/registro');
-                    },
-                    child: Text("SIGN UP",
-                        style: TextStyle(color: Colors.grey, fontSize: 10)),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 20,
-              ),
-              child: Text("Fast Reservation",
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 102, 105, 110), fontSize: 30)),
-            ),
-            SizedBox(
-              width: 20,
-              height: 10,
-            ),
-            SizedBox(
-              width: 370,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Nombre de Usuario',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40)),
+          height: double.infinity,
+          width: double.infinity,
+          color: Colors.transparent,
+          child: ListView(
+            children: [
+              Container(
+                alignment: Alignment.topCenter,
+                width: double.infinity,
+                height: 200,
+                child: SvgPicture.asset(
+                  color: Color(0xFF80DDFF),
+                  'assets/wave.svg',
+                  alignment: Alignment.bottomCenter,
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height,
+                  fit: BoxFit.fill,
                 ),
               ),
-            ),
-            SizedBox(
-              width: 20,
-              height: 10,
-            ),
-            SizedBox(
-              width: 370,
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
+              Container(
+                color: Colors.transparent,
+                margin: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.10),
+                child: Row(
+                  children: [
+                    Text(
+                      "SIGN IN ",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "/",
+                      style: TextStyle(color: Colors.grey, fontSize: 15),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/registro');
+                      },
+                      child: Text("SIGN UP",
+                          style: TextStyle(color: Colors.grey, fontSize: 10)),
+                    )
+                  ],
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 20,
+                  ),
+                  child: Text("Fast Reservation",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 102, 105, 110),
+                          fontSize: 30)),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+                height: 10,
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 20.0, left: 20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nombre de Usuario',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+                height: 10,
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 20.0, left: 20.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
                     labelText: 'Contraseña',
                     border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(40))),
+                        borderRadius: BorderRadius.circular(40)),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(
-              width: 20,
-              height: 50,
-            ),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Container(
-                  width: 370,
+              SizedBox(
+                width: 20,
+                height: 50,
+              ),
+              Container(
                   height: 50,
+                  margin: EdgeInsets.all(20),
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(40),
                       gradient: LinearGradient(colors: [
                         Color(0xFF00BBFF),
                         Color(0xFF80DDFF),
                         Color(0xFF00BBFF),
-                        //Color(0xFF005E80),
                       ])),
                   child: TextButton(
                     onPressed: () {},
                     child: Text("Ingresar",
                         style: TextStyle(color: Colors.white, fontSize: 15)),
                   )),
-            ),
-          ]),
-        ),
-      ),
+              SizedBox(
+                width: 20,
+                height: 50,
+              ),
+            ],
+          )),
     );
   }
 }
